10440afa2c2eece0edaf6adf5fcc417726474673
Joe Flateau <joe@joeflateau.net>
2022-07-26 14:02:13 -0400

    feat: thin wrapper around cdk-ec2-key-pair for generating jwt keypairs for use in ecs tasks

---
+++ packages/ecs-jwt-keypair/README.md
@@ -0,0 +1,11 @@
+# ecs-jwt-keypair
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test ecs-jwt-keypair` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Running lint
+
+Run `nx lint ecs-jwt-keypair` to execute the lint via [ESLint](https://eslint.org/).
---
+++ packages/ecs-jwt-keypair/src/lib/ecs-jwt-keypair.ts
@@ -0,0 +1,46 @@
+import { Secret as EcsSecret } from 'aws-cdk-lib/aws-ecs';
+import { ISecret, Secret } from 'aws-cdk-lib/aws-secretsmanager';
+import { KeyPair, PublicKeyFormat } from 'cdk-ec2-key-pair';
+import { Construct } from 'constructs';
+
+export class EcsJwtKeyPair extends Construct {
+  keyPair: KeyPair;
+
+  secrets: {
+    privateKey: ISecret;
+    publicKey: ISecret;
+  };
+
+  ecsSecrets: {
+    privateKey: EcsSecret;
+    publicKey: EcsSecret;
+  };
+
+  constructor(scope: Construct, id: string, props: { keyName?: string }) {
+    super(scope, id);
+
+    this.keyPair = new KeyPair(this, 'KeyPair', {
+      name: props.keyName ?? this.node.addr,
+      storePublicKey: true,
+      publicKeyFormat: PublicKeyFormat.PEM,
+    });
+
+    this.secrets = {
+      privateKey: Secret.fromSecretCompleteArn(
+        this,
+        'JwtPrivateKey',
+        this.keyPair.privateKeyArn
+      ),
+      publicKey: Secret.fromSecretCompleteArn(
+        this,
+        'JwtPublicKey',
+        this.keyPair.publicKeyArn
+      ),
+    };
+
+    this.ecsSecrets = {
+      privateKey: EcsSecret.fromSecretsManager(this.secrets.privateKey),
+      publicKey: EcsSecret.fromSecretsManager(this.secrets.publicKey),
+    };
+  }
+}
