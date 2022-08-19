36476ac0d97e7ee0f4e85664da87b84e827da77b
Joe Flateau <joe@joeflateau.net>
2022-08-19 10:52:19 -0400

    fix(image-resize-behavior): remove client-dynamodb dependency that's already provided as a peer of lib-dynamodb

--- packages/image-resize-behavior/src/lib/image-resize-behavior.ts
+++ packages/image-resize-behavior/src/lib/image-resize-behavior.ts
@@ -72,12 +72,7 @@ export class ImageResizeBehavior extends Construct {
       {
         bundling: {
           minify: true,
-          nodeModules: [
-            'sharp',
-            '@aws-sdk/client-s3',
-            '@aws-sdk/lib-dynamodb',
-            '@aws-sdk/client-dynamodb',
-          ],
+          nodeModules: ['sharp', '@aws-sdk/client-s3', '@aws-sdk/lib-dynamodb'],
           commandHooks: {
             beforeInstall: () => [],
             beforeBundling: () => [],
